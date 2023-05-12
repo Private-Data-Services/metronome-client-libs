c89b039200093b599992335f5a17b17f354f986c
robin <robin108@gmail.com>
2023-05-12 15:06:15 +0200

    Expiry notices example improvement.

--- typescript-examples/src/data-expiry-examples.ts
+++ typescript-examples/src/data-expiry-examples.ts
@@ -10,11 +10,21 @@ const queryExpiriesExample = async () => {
     try {
         const response = await expiryApiClient.getExpiryNotices("20230809");
         
-        response.complete.forEach( (notice) => {
+        // Pending expiries may change with use before the currently reported date.  They are advisory only.
+        response.pending.forEach( (notice) => {
             if(notice.itemId) {
                 console.log("Item %s expires on %s", notice.itemId, response.expiryDate);
             } else {
                 console.log("Item %s has sub-items %s expirying on %s", notice.parentItemId, JSON.stringify(notice.subItems), response.expiryDate);
+            }
+        });
+
+        // Complete expiries indicate that the data must be deleted.  You must act on complete expiry notices.
+        response.complete.forEach( (notice) => {
+            if(notice.itemId) {
+                console.log("Item %s did expire on %s", notice.itemId, response.expiryDate);
+            } else {
+                console.log("Item %s has sub-items %s that did expire on %s", notice.parentItemId, JSON.stringify(notice.subItems), response.expiryDate);
             }
         });
     } catch (err) {
